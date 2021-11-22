a738b824f76824cee0034fd3f52c6107005c6f81
918127726 <92682019+918127726@users.noreply.github.com>
2021-11-22 13:33:21 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,14 +9,14 @@ else:
 
 
 class Config:
-    API_ID = int(getenv("API_ID", "0"))
-    API_HASH = getenv("API_HASH", "abc123")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234:abcd")
+    API_ID = int(getenv("API_ID", "6435225"))
+    API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d")
+    BOT_TOKEN = getenv("BOT_TOKEN", "")
     SESSION = getenv("SESSION", "session")
-    OWNER_ID = int(getenv("OWNER_ID", "1952053555"))
-    SUPPORT = getenv("SUPPORT", "https://t.me/solidprojects_chat")
-    CHANNEL = getenv("CHANNEL", "https://t.me/solidprojects")
-    UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/DoellBarr/solidmusic")
+    OWNER_ID = int(getenv("OWNER_ID", "1669178360"))
+    SUPPORT = getenv("SUPPORT", "https://t.me/Superior_support")
+    CHANNEL = getenv("CHANNEL", "https://t.me/Superior_bots")
+    UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/itsunknown-12")
 
 
 config = Config()
